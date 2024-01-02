e11f86328706063e5ea7ff7e902adc13c7e55449
Sai0698 <sairam.kotha@icloud.com>
2024-01-02 18:42:05 +0530

    calling response

--- app/src/main/java/com/example/project004/MainActivity.kt
+++ app/src/main/java/com/example/project004/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.project004
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project004.databinding.ActivityMainBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -10,19 +12,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val word="world"
-        GlobalScope.launch {
-
-            val response= RetrofitInstance.apiInterface.dictcall(word)
-            runOnUiThread {
-
-                response.body()?.first()?.let{
-                    binding.hello.text= it.phonetics[1].toString()
-                }
-            }
+        binding.searchBtn.setOnClickListener {
+            val word = binding.etTxt.text.toString()
+            callApi(word)
         }
-
     }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun callApi(word: String) {
+        GlobalScope.launch {
+            val response = RetrofitInstance.apiInterface.dictcall(word)
+            Log.i("responsefromApi",response.body().toString())
+        }
+    }
+
 }
